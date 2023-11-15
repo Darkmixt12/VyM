35136efa165d34f35b50ed324bd9de89b1f5ee18
Steven Muñoz <darkmixt@gmail.com>
2023-11-15 16:29:54 -0600

    fix : TL request

--- libs/vym/frontend/ui/app/src/lib/components/app.component.ts
+++ libs/vym/frontend/ui/app/src/lib/components/app.component.ts
@@ -3,14 +3,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '@vym/libs/frontend/shared/footer';
 
-
 @Component({
   selector: 'vym-app',
   standalone: true,
-  imports: [
-    CommonModule,
-    FooterComponent
-  ],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
