3f288bc3fb5a77c0b3c41e300a159dbee237f54c
Steven Muñoz <darkmixt@gmail.com>
2023-12-21 10:39:22 -0600

    feat: all problem fixed safe point

--- libs/vym/frontend/ui/facturas-despacho/src/lib/components/facturas-despacho/facturas-despacho.component.ts
+++ libs/vym/frontend/ui/facturas-despacho/src/lib/components/facturas-despacho/facturas-despacho.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DespachoComponent } from '../despacho.component';
-import { InitialFormComponent } from '../initial-form/initial-form.component';
 
 @Component({
   selector: 'vym-facturas-despacho',
   standalone: true,
-  imports: [CommonModule, DespachoComponent, InitialFormComponent],
+  imports: [CommonModule, DespachoComponent],
   templateUrl: './facturas-despacho.component.html',
   styleUrls: ['./facturas-despacho.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
